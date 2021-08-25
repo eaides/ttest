437d8d2d4d41ea8e27a66ad9a81433d56187b861
Ernesto Aides <eaides@hotmail.com>
2021-08-25 20:33:14 +0300

    add some comments

--- app/Helpers/ResponseTime.php
+++ app/Helpers/ResponseTime.php
@@ -5,7 +5,7 @@ namespace App\Helpers;
 class ResponseTime
 {
     /**
-     * @brief give response time (readable in seconds) for a given $url or error message
+     * @brief give response time (human readable in seconds) for a given $url or error message
      *
      * @param string $url
      * @return bool|string
@@ -15,19 +15,26 @@ class ResponseTime
         if (!is_string($url)) return false;
         if (empty($url)) return false;
 
-        try {
+        try
+        {
+            // using curl to try to retrieve the url page
             $ch = curl_init($url);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
             curl_exec($ch);
-        } catch (\Exception $e) {
+        }
+        catch (\Exception $e)
+        {
+            // for any exception, return the message of the exception
             return $e->getMessage();
         }
         $info = curl_getinfo($ch);
         $error = curl_error($ch);
         if($error)
         {
+            // if was an error (like url not exists) return the received error
             return (string)$error;
         }
+        // return in human readable the response time in seconds
         return 'Took ' . $info['total_time'] . ' seconds to transfer a request to ' . $info['url'];
     }
 }
